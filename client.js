ac39194d3a0502b2f3ba9f8c19b9ff042dcb9b3d
LazyDotExe <papaporo@proton.me>
2023-07-11 16:02:52 -0500

    completed some of the functions

--- client.js
+++ client.js
@@ -49,9 +49,69 @@ console.log('array of employee data: ',  employees );
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
+  // console.log(employee);
   // your logic here
+  let bonusPercentage = 0;
+  const salaryNumber = Number(employee.annualSalary)
+
+
+
+if (employee.reviewRating <= 2) {
+  bonusPercentage = 0;
+}
+if (employee.reviewRating == 3) {
+  bonusPercentage = 4;
+}
+if (employee.reviewRating == 4) {
+  bonusPercentage = 6;
+}
+if (employee.reviewRating == 5) {
+  bonusPercentage = 10;
+}
+if (employee.employeeNumber.length == 4) {
+  bonusPercentage += 5
+}
+if (employee.annualSalary > 65000) {
+  bonusPercentage -= 1
+}
+if (bonusPercentage > 13) {
+  bonusPercentage = 13
+}
+if (bonusPercentage < 0) {
+  bonusPercentage = 0
+} 
+
+
+
+
+
+
+
+let bonusDecimal = bonusPercentage/100
+let totalBonus = salaryNumber * bonusDecimal
+console.log(totalBonus);
+
+let totalCompensation = totalBonus + salaryNumber
+
+
+
   
+  let result = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: 'total bonus - TBD'
+
+  }
+  return result
   
   // return new object with bonus results
 
+}
+
+
+// Testing for all employees
+for (let individual of employees) {
+  console.log(`${individual.name}`)
+  console.log(calculateIndividualEmployeeBonus(individual))
 }
